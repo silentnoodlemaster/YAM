5b57c4b73bb6f2b8bf84125b27a158b39e397a5d
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2020-12-19 09:16:41 +0100

    Add all production dependencies

--- tools/snapshot.js
+++ tools/snapshot.js
@@ -5,8 +5,15 @@
 
 require("electron-updater");
 require("electron-store");
+// require("electron-is-dev"); Gives warning
 // require("f95api"); Error with variables with # in the name (private)
 require("simple-image-downloader");
 require("imagemin");
 require("imagemin-webp");
 require("imagemin-gifsicle");
+require("i18next");
+require("i18next-electron-language-detector");
+require("string-similarity");
+require("new-github-issue-url");
+// require("nedb-promises"); Gives warning
+require("ajv");
